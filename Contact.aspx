98a9c001b55b50897ffe5e3e41dcbceadfff5cfd
prajwal2117 <119020725+prajwal2117@users.noreply.github.com>
2023-07-15 09:03:17 +0530

    Add files via upload

---
+++ Contact.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="WebApplication1.Contact" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:HiddenField ID="hfContactID" runat="server" />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+
+                    </td>
+                    </tr>
+                <tr>
+                   
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="Mobile"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtMobile" runat="server"></asp:TextBox>
+                    </td>
+                    </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label3" runat="server" Text="Address"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtAddress" runat="server" TextMode="MultiLine"></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                       
+                    </td>
+                    <td colspan="2">
+                        <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                        <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                       
+                    </td>
+                    <td colspan="2">
+                        <asp:Label ID="lblSuccessMessage" runat="server" forecolor="Green" Text=""></asp:Label>
+                    </td>
+                </tr> <tr>
+                    <td>
+                       
+                    </td>
+                    <td colspan="2">
+                        <asp:Label ID="lblError" runat="server" ForeColor="Red" Text=""></asp:Label>
+                    </td>
+                </tr>
+
+            </table>
+            <br />
+            <asp:GridView ID="gvContact" runat="server" AutoGenerateColumns="false" Width="335px">
+                <Columns>
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Mobile" HeaderText="Mobile" />
+                    <asp:BoundField DataField="Address" HeaderText="Address" />
+
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lnkView" runat="server" CommandArgument='<%# Eval("ContactID") %>' OnClick="lnk_onClick">View</asp:LinkButton>
+                            
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Contact.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+
+        SqlConnection SqlCon = new SqlConnection(@"Data Source =DESKTOP-VOE8H6R; Initial Catalog = ASPCRUD; Integrated Security = true"); 
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnDelete.Enabled = false;
+                FillGridView();
+            }
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+
+        }
+
+        public void Clear()
+        {
+            hfContactID.Value = "";
+            txtName.Text = txtMobile.Text = txtAddress.Text = "";
+            lblSuccessMessage.Text = lblError.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (SqlCon.State == ConnectionState.Closed)
+                SqlCon.Open();
+            SqlCommand SqlCmd = new SqlCommand("ContactCreateOrUpdate", SqlCon);
+            SqlCmd.CommandType = CommandType.StoredProcedure;
+            SqlCmd.Parameters.AddWithValue("@ContactID", (hfContactID.Value == "" ? 0 : Convert.ToInt32(hfContactID.Value)));
+            SqlCmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+            SqlCmd.Parameters.AddWithValue("@Mobile", txtMobile.Text.Trim());
+            SqlCmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+            SqlCmd.ExecuteNonQuery();
+            SqlCon.Close();
+            string ContactID = hfContactID.Value;
+            Clear();
+            if (ContactID == "")
+                lblSuccessMessage.Text = "Saved Successfully";
+            else
+                lblSuccessMessage.Text = "Updated Successfully";
+            FillGridView();
+
+
+        }
+
+        void FillGridView()
+        {
+            if (SqlCon.State == ConnectionState.Closed)
+                SqlCon.Open();
+            SqlDataAdapter SqlDa = new SqlDataAdapter("ContactViewall", SqlCon);
+            SqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dtbl = new DataTable();
+            SqlDa.Fill(dtbl);
+            SqlCon.Close();
+            gvContact.DataSource = dtbl;
+            gvContact.DataBind();
+
+        }
+
+        protected void lnk_onClick(Object sender, EventArgs e)
+        {
+          int ContactID=Convert.ToInt32((sender as LinkButton).CommandArgument);
+            if (SqlCon.State == ConnectionState.Closed)
+                SqlCon.Open();
+            SqlDataAdapter SqlDa = new SqlDataAdapter("ContactViewByID", SqlCon);
+            SqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            SqlDa.SelectCommand.Parameters.AddWithValue("@ContactID", ContactID);
+            DataTable dtbl = new DataTable();
+            SqlDa.Fill(dtbl);
+            SqlCon.Close();
+            hfContactID.Value = ContactID.ToString();
+            txtName.Text = dtbl.Rows[0]["Name"].ToString();
+            txtMobile.Text = dtbl.Rows[0]["Mobile"].ToString();
+            txtAddress.Text = dtbl.Rows[0]["Address"].ToString();
+            btnSave.Text = "Update";
+            btnDelete.Enabled = true;
+
+
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (SqlCon.State == ConnectionState.Closed)
+                SqlCon.Open();
+            SqlCommand SqlCmd = new SqlCommand("ContactDeleteByID",SqlCon);
+            SqlCmd.CommandType = CommandType.StoredProcedure;
+            SqlCmd.Parameters.AddWithValue("@ContactID", Convert.ToInt32(hfContactID.Value));
+            SqlCmd.ExecuteNonQuery();
+            SqlCon.Close();
+            Clear();
+            FillGridView();
+            lblSuccessMessage.Text = "Delete Successfully";
+        }
+    }
+}
